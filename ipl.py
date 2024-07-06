f1276bff882227fe3153708940ca62bc47d72e1e
KrishnaG Thakur <kgopa0204@gmail.com>
2024-07-06 10:44:09 +0530

    add teams api

---
+++ ipl.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+
+matches = pd.read_csv('ipl-matches.csv')
+
+def teamsAPI():
+    teams = list(set((list(matches["Team1"]) + list(matches["Team2"]))))
+    team_dict = {
+        "teams" : teams
+    }
+    return team_dict
+
