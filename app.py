35bf1684d26f828eb585707e71e0aa38569f2c73
KrishnaG Thakur <kgopa0204@gmail.com>
2024-07-06 13:50:16 +0530

    add team vs team api

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,jsonify
+from flask import Flask, jsonify, request
 import ipl
 
 app = Flask(__name__)
@@ -12,5 +12,12 @@ def teams():
     teams = ipl.teamsAPI()
     return jsonify(teams)
 
+@app.route('/api/teamvsteam')
+def teamvsteam():
+    team1 = request.args.get('team1')
+    team2 = request.args.get('team2')
+    response = ipl.team_vs_team(team1, team2)
+
+    return jsonify(response)
 
 app.run(debug=True)
--- ipl.py
+++ ipl.py
@@ -10,3 +10,18 @@ def teamsAPI():
     }
     return team_dict
 
+def team_vs_team(team1, team2):
+    temp_df = matches[((matches['Team1'] == team1) & (matches['Team2'] == team2)) | (
+                (matches['Team1'] == team2) & (matches['Team2'] == team1))]
+    total_matches = temp_df.shape[0]
+    matches_won_team1 = temp_df["WinningTeam"].value_counts()[team1]
+    matches_won_team2 = temp_df["WinningTeam"].value_counts()[team2]
+    draws = total_matches - (matches_won_team1 + matches_won_team2)
+
+    result = {
+        'total_matches': str(total_matches),
+        team1: str(matches_won_team1),
+        team2: str(matches_won_team2),
+        'draws': str(draws)
+    }
+    return result
